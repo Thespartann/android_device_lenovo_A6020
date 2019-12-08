e6423f6037ecba8f3849b0b47143308926df9a97
Thespartann <danilastefan69@yahoo.ro>
2019-12-08 14:54:05 +0200

    A6020: Enable dex-preopt
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

---
+++ board/build.mk
@@ -0,0 +1,9 @@
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_PIC := true
+    endif
+  endif
+endif
